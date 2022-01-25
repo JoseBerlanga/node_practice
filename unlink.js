d97ca4ed2ca2cf7f51e6e59293990b333e4ba37a
joseberlanga <jjcberlanga_91@hotmail.com>
2022-01-25 14:53:37 -0600

    unlink2

--- unlink.js
+++ unlink.js
@@ -1,13 +1,14 @@
 const fs = require ('fs')
 
 function result(error) {
-    if (error) {
-        console.log('hubo un error', error)
+    if (!error) {
+        console.log('El archivo fue escito exitosamente')
         return
     }
-    console.log('El archivo fue escito exitosamente')
+    console.log(`Error: ${error}`)
 }
 
+
 fs.writeFile('hola2.txt', 'Archivo unlink', result)
-fs.appendFile('hola2.txt', 'Mi nombre', result)
+fs.appendFile('hola2.txt', '-Mi nombre', result)
 fs.unlink('hola2.txt', result)
